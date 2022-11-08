a84eb86bc57b625f3a29e66ddf294e377a94fcf6
elch <chiosso.ez@gmail.com>
2022-11-08 00:55:16 -0300

    v2-solucion de errores

--- adivinar.js
+++ adivinar.js
@@ -1,6 +1,7 @@
 let numEleg =0;
 let numAleat=0;
 let continuar2;
+let promedioSuerte=0, cantVueltas=0;
 let cV=0; //contador de vueltas
 
 class Azar {
@@ -11,7 +12,6 @@ class Azar {
     }
 }
 
-const vuelta = [];
 
 function aleat(inferior, superior) {
     let numPosibilidades = superior - inferior;
@@ -20,6 +20,8 @@ function aleat(inferior, superior) {
     return inferior + aleatorio;
 }
 
+
+const vuelta = [];
 
 do {
     alert("Bienvenido al Adivinador... vamos a probar tu suerte");
@@ -43,73 +45,73 @@ do {
             vuelta[cV].fracasos++;
         }
     
-        suerte = (vuelta[cV].aciertos/i)*10;
+        vuelta[cV].suerte = (vuelta[cV].aciertos/i)*10;
         switch (i) {
             case 1:
-                if (suerte==10)
+                if (vuelta[cV].suerte==10)
                     alert("Arrancaste demasiado bien.. ¿Sos algo de HarryPotter?");
-                if (suerte==0)
+                if (vuelta[cV].suerte==0)
                     alert("Bienvenido a las desilusiones de la vida");
                 break;
     
             case 2:
-                if (suerte==10)
+                if (vuelta[cV].suerte==10)
                     alert("No te la puedo creer!!");
-                if (suerte==5)
+                if (vuelta[cV].suerte==5)
                     alert("Una de cal y una de arena. Bien ahi");
-                if (suerte==0)
+                if (vuelta[cV].suerte==0)
                     alert("Todavia tenes tiempo");
                 break;
     
             case 3:
-                if (suerte==10)
+                if (vuelta[cV].suerte==10)
                     alert("Brujería!!");
-                if (suerte<10 && suerte >=5)
+                if (vuelta[cV].suerte<10 && vuelta[cV].suerte >=5)
                     alert("Vas bien!");
-                if (suerte>0 && suerte <5)
+                if (vuelta[cV].suerte>0 && vuelta[cV].suerte <5)
                     alert("Podria ser mejor");
-                if (suerte==0)
+                if (vuelta[cV].suerte==0)
                     alert("Espero que por lo menos seas carismatico");
                 break;
     
             case 4:
-                if (suerte==10)
+                if (vuelta[cV].suerte==10)
                     alert("Deja de estudiar y anda al casino!!!");
-                if (suerte==7.5)
+                if (vuelta[cV].suerte==7.5)
                     alert("Por favor dejame ser tu amigo!");
-                if (suerte==5)
+                if (vuelta[cV].suerte==5)
                     alert("Nada mal!!");
-                if (suerte==2.5)
+                if (vuelta[cV].suerte==2.5)
                     alert("Te recomiendo estudiar porque de suerte estas mas o menos");
-                if (suerte==0)
+                if (vuelta[cV].suerte==0)
                     alert("Ya ni el carisma te salva");
                 break;
     
             case 4:
-                if (suerte==10)
+                if (vuelta[cV].suerte==10)
                     alert("Deja de estudiar y anda al casino!!!");
-                if (suerte==7.5)
+                if (vuelta[cV].suerte==7.5)
                     alert("Por favor dejame ser tu amigo!");
-                if (suerte==5)
+                if (vuelta[cV].suerte==5)
                     alert("Nada mal!!");
-                if (suerte==2.5)
+                if (vuelta[cV].suerte==2.5)
                     alert("Consolate con que hay algunos peor que vos");
-                if (suerte==0)
+                if (vuelta[cV].suerte==0)
                     alert("espero tengas buenos contactos..");
                 break;
     
             case 5:
-                if (suerte==10)
+                if (vuelta[cV].suerte==10)
                     alert("Realmente impresionante! Lo tuyo no es humano");
-                if (suerte==8)
+                if (vuelta[cV].suerte==8)
                     alert("Por favor dejame ser tu amigo!");
-                if (suerte==6)
+                if (vuelta[cV].suerte==6)
                     alert("Bastante bien");
-                if (suerte==4)
+                if (vuelta[cV].suerte==4)
                     alert("Un mortal como todos");
-                if (suerte==2)
+                if (vuelta[cV].suerte==2)
                     alert("Supongo que en el amor te va bien");
-                if (suerte==0)
+                if (vuelta[cV].suerte==0)
                     alert("La tenes complicada.. anda buscando alguna herencia por ahi");
                 break;
     
@@ -117,20 +119,22 @@ do {
                 break;
         }
     
-        console.log("Suerte acumulada del 0 al 100: " +(suerte*10)+ " %");
+        console.log("Suerte acumulada del 0 al 100: " +(vuelta[cV].suerte*10)+ " %");
         numEleg=0;
     }
     
-    cV++;
-
-    console.log("Aciertos: " + aciertos);
-    console.log("Fracasos: " + fracasos);
-    console.log("Suerte: " +(suerte*10)+ " %");
+    console.log("Aciertos: " + vuelta[cV].aciertos);
+    console.log("Fracasos: " + vuelta[cV].fracasos);
+    console.log("Suerte: " +(vuelta[cV].suerte*10)+ " %");
     
     continuar2 = prompt("Desea probar de nuevo? (si/no)")
+    cV++;
 
 } while (continuar2 == "si");
 
+promedioSuerte= vuelta.reduce((acc, vuelta) => acc += vuelta.suerte, 0);
+cantVueltas = vuelta.length;
+console.log("Suerte acumulada: " +(promedioSuerte*10/cantVueltas)+ " %");
 
 
 
